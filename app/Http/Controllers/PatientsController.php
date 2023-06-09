bad6bfc7918192a9fb66e232e18be0d25fe247aa
Joe Asewe <joeasewe@gmail.com>
2023-06-10 00:20:28 +0300

    gender and service models fix

--- app/Http/Controllers/PatientsController.php
+++ app/Http/Controllers/PatientsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Patient;
+use App\Models\Gender;
+use App\Models\Service;
 
 class PatientsController extends Controller
 {
